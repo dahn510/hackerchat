144947509ffbe38410cddafdf6fbc419afe2b95f
dahn510 <dahn510@gmail.com>
2022-01-31 15:16:13 -0800

    implemented a message module

---
+++ include/hackerchat/net.h
@@ -0,0 +1,7 @@
+#ifndef HACKERCHAT_NET_HPP
+#define HACKERCHAT_NET_HPP
+
+#include <hackerchat/net_common.h>
+#include <hackerchat/net_message.h>
+
+#endif
---
+++ include/hackerchat/net_common.h
@@ -0,0 +1,24 @@
+#ifdef HACKERCHAT_NET_COMMON_HPP
+#define HACKERCHAT_NET_COMMON_HPP
+
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <optional>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+typedef uint32_t u_int32_t;
+
+
+
+#endif
