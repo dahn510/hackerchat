785ec9a8bbd48109f688e6546f4ef11f0245a22a
dahn510 <dahn510@gmail.com>
2022-02-10 12:24:19 -0800

    Small progress so far, still learning about ASIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
-#include <hackerchat/version.hpp>
 #include <iostream>
 
+#include <hackerchat/net.h>
+#include <hackerchat/version.hpp>
+
+enum class CustomMessageTypes: uint32_t
+{
+	From,
+	Message
+};
 int main()
 {
-	std::cout << "Hackerchat v" << hackerchat::get_version() << std::endl;
+	hackerchat::net::message<CustomMessageTypes> msg;
+	msg.header.id = CustomMessageTypes::From;
+
+	int a = 1;
+	bool b = true;
+	float c = 3.123123;
+	
+	struct 
+	{
+		float x;
+		float y;
+	} d[5];
+
+	msg << a << b << c << d;
+
+	a = 99;
+	b = false;
+	c = 99.0;
+
+	msg >> d >> c >> b >> a;
+
+	std::cout << "msg >> "<<  d << " >> " <<  c << " >> " << b << " >> " << a << std::endl;
+
+	return 0;
 }
